f61c3b3df23ecb1263edc716fa0fc3345d12e722
fnmarcos <127150588+fnmarcos@users.noreply.github.com>
2023-05-20 14:30:31 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,65 @@
+// Parte 1
+
+// var topping = "Oreo";
+// var precio = 0;
+// var precioFinal;
+// var helado = 10;
+
+// if (topping == "Oreo") {
+//     precio = 10;
+// } else if (topping == "KitKat") {
+//     precio = 15;
+// } else if (topping == "Kinder") {
+//     precio = 25;
+// } else
+//     console.log("No tenemos este topping")
+
+// precioFinal = precio + helado
+
+// console.log("El helado cuesta $: " + precioFinal);
+
+// Parte 2
+
+// var menu ="";
+// var bebida;
+
+
+// switch (menu) {
+//     case "carne":
+//         bebida = "vino tinto";
+//         console.log("El menu es " + menu + " con " + bebida);
+//         break;
+//     case "pescado":
+//         bebida = "vino blanco";
+//         console.log("El menu es " + menu + " con " + bebida);
+//         break;
+//     case "verdura":
+//         bebida = "agua";
+//         console.log("El menu es " + menu + " con " + bebida);
+//         break;
+//     default:
+//         console.log("Elija carne, pescado o verdura")
+// }
+
+// Parte 3
+// var frutas = ["Manzana", "Banana", "Mandarina", "Uva"]
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log("El elemento es: " + frutas[i])
+// }
+
+// frutas.push("Pera");
+// console.log("Nueva lista de frutas:")
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log("El elemento es: " + frutas[i])
+// }
+
+// Parte 4
+
+// var num = 0;
+
+// while (num < 11) {
+//     console.log("El numero " + num + " es más chico que 11");
+//     num++;
+// }
